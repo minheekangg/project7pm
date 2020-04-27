db6066e332ab32582fcb18df7a17967b209b48aa
minheekangg <minheekangg@gmail.com>
2020-04-27 18:48:15 -0400

    added front and back image

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ $(document).ready(function () {
         var section = $(this).attr("href");
         $("html, body").animate({
             scrollTop: $(section).offset().top - 100
-        }, 2000);
+        }, 1500);
     });
+    
+
+    function debounce(func, wait = 20, immediate = true) {
+        var timeout;
+        return function () {
+            var context = this, args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    }
+
+    $(".product").hover(debounce(function (e){
+        console.log(e.target.dataset.backimg)
+        $(this).css('background-image', 'url(' + e.target.dataset.backimg + ')')
+    }))
+    $(".product").mouseleave(debounce(function (e){
+        console.log(e.target.dataset.frontimg)
+        $(this).css('background-image', 'url(' + e.target.dataset.frontimg + ')')
+    }))
 });
